4e6462674ab065e65319aeb15646bc376a650552
galaran <galaran.h@gmail.com>
2020-12-23 04:09:01 +0300

    Log player death locations

--- src/main/kotlin/me/galaran/nyamine/feature/DeathLocation.kt
+++ src/main/kotlin/me/galaran/nyamine/feature/DeathLocation.kt
@@ -1,9 +1,12 @@
 package me.galaran.nyamine.feature
 
+import me.galaran.nyamine.LOGGER
 import me.galaran.nyamine.NyaMineFeatures
 import me.galaran.nyamine.Position
-import net.md_5.bungee.api.ChatColor
-import net.md_5.bungee.api.chat.ComponentBuilder
+import me.galaran.nyamine.util.color
+import me.galaran.nyamine.util.plus
+import net.md_5.bungee.api.ChatColor.RED
+import net.md_5.bungee.api.ChatColor.YELLOW
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -14,12 +17,14 @@ class DeathLocation(private val plugin: NyaMineFeatures) : Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val deathLoc = event.entity.location
-        event.entity.sendMessage(*ComponentBuilder("Death point: ").color(ChatColor.YELLOW)
-                .append("${deathLoc.blockX} ${deathLoc.blockY} ${deathLoc.blockZ}").color(ChatColor.RED)
-                .append(" at ").color(ChatColor.YELLOW)
-                .append(deathLoc.world.name).color(ChatColor.RED)
-                .create()
-        )
+
+        val message = "Death location: ".color(YELLOW) +
+                "${deathLoc.blockX} ${deathLoc.blockY} ${deathLoc.blockZ}".color(RED) +
+                " at ".color(YELLOW) + deathLoc.world.name.color(RED)
+
+        event.entity.sendMessage(message)
+        LOGGER.info("Player ${event.entity.name} died. " + message.toPlainText())
+
 
         plugin.playerStorage[event.entity].lastDeathPoint =
                 if (deathLoc.y >= 0) Position(deathLoc.x, deathLoc.y, deathLoc.z) else null  // Do not save when fall down to void
