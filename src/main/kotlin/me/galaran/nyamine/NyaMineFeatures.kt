bb1c0ccf7155da0d9e76f9ad224ee11afdd1ec2c
galaran <galaran.h@gmail.com>
2021-01-09 02:01:22 +0300

    Remove converter from legacy player storage

--- src/main/kotlin/me/galaran/nyamine/NyaMineFeatures.kt
+++ src/main/kotlin/me/galaran/nyamine/NyaMineFeatures.kt
@@ -1,26 +1,16 @@
 package me.galaran.nyamine
 
 import com.earth2me.essentials.Essentials
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.modules.contextual
 import me.galaran.nyamine.command.AdminCommand
 import me.galaran.nyamine.command.ChorusCommand
 import me.galaran.nyamine.command.NyaCommandDispatcher
 import me.galaran.nyamine.command.PlayerInfoCommand
 import me.galaran.nyamine.feature.*
-import me.galaran.nyamine.storage.BasePlayerData
 import me.galaran.nyamine.storage.PlayerStorageDirectory
-import me.galaran.nyamine.storage.PlayerStorageSingleFile
-import me.galaran.nyamine.storage.data.Location
-import me.galaran.nyamine.storage.data.Vector
-import me.galaran.nyamine.storage.data.WorldType
 import net.ess3.api.IEssentials
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
-import java.nio.file.Files
-import java.util.*
 
 
 class NyaMineFeatures : JavaPlugin() {
@@ -45,7 +35,6 @@ class NyaMineFeatures : JavaPlugin() {
         OfflinePlayerRegistry.init(server.offlinePlayers)
 
         playerStorage = PlayerStorageDirectory(dataFolder.toPath().resolve("players"))
-        convertLegacyStorage()
 
         CustomItems.Recipies.registerAll()
         server.pluginManager.registerEvents(CustomItems.Recipies.Discoverer(), this)
@@ -77,45 +66,6 @@ class NyaMineFeatures : JavaPlugin() {
         }, 5 * 60 * 20, 5 * 60 * 20)  // 5 min
 
         logger.info("NyaMineFeatures enabled")
-    }
-
-    @Deprecated("Temp")
-    private fun convertLegacyStorage() {
-        val legacyStoragePath = dataFolder.toPath().resolve("PlayerStorage.json")
-        if (!Files.exists(legacyStoragePath)) return
-
-        LOGGER.info("Converting legacy player storage to directory format")
-
-        val playerStorageLegacy = PlayerStorageSingleFile(
-            legacyStoragePath,
-            ::PlayerDataLegacy,
-            PlayerDataLegacy.serializer(),
-            SerializersModule {
-                contextual(Vector.serializer())
-            }
-        )
-        playerStorageLegacy.reload()
-
-        // convert to new format
-        var counter = 0
-        playerStorageLegacy.value.byPlayer.forEach { (uuidString, legacyData) ->
-            legacyData.lastDeathPoint?.let {
-                val newFormat = playerStorage.get(UUID.fromString(uuidString), legacyData.playerName)
-                newFormat.lastDeathPoint = Location(it.x, it.y, it.z, "NyaBees", WorldType.OVERWORLD)
-                counter++
-            }
-        }
-        playerStorage.saveAll()
-
-        Files.move(legacyStoragePath, dataFolder.toPath().resolve("PlayerStorage.converted.json"))
-
-        LOGGER.info("Success. Converted $counter player profiles to new format")
-    }
-
-    @Serializable
-    @Deprecated("To replace with PlayerStorageDirectory")
-    class PlayerDataLegacy : BasePlayerData() {
-        var lastDeathPoint: Vector? = null
     }
 
     fun reloadConf() {
