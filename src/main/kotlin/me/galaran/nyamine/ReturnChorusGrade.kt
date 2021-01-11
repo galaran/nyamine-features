601edafee6ef14b7b4e9efe084be6b5a30ac3dbc
galaran <galaran.h@gmail.com>
2021-01-11 03:46:24 +0300

    ReturnChorus: Add progress bar

--- src/main/kotlin/me/galaran/nyamine/ReturnChorusGrade.kt
+++ src/main/kotlin/me/galaran/nyamine/ReturnChorusGrade.kt
@@ -1,15 +1,17 @@
 package me.galaran.nyamine
 
 import org.bukkit.ChatColor
+import org.bukkit.boss.BarColor
 
 enum class ReturnChorusGrade(
         val displayName: String,
         val lore: String?,
         val nameColor: ChatColor,
+        val progressBarColor: BarColor?,
         val enchantLevel: Int,
         val returnDelayTicks: Int
 ) {
-    COMMON("Хорус возвращения", null, ChatColor.YELLOW, 1, 240),
-    FAST("Хорус быстрого возвращения", null, ChatColor.AQUA, 5, 70),
-    INSTANT("Хорус мгновенного возвращения", "хоме! Хоме! ХОМЕ!", ChatColor.LIGHT_PURPLE, 10, 2)
+    COMMON("Хорус возвращения", null, ChatColor.YELLOW, BarColor.YELLOW, 1, 240),
+    FAST("Хорус быстрого возвращения", null, ChatColor.AQUA, BarColor.BLUE, 5, 70),
+    INSTANT("Хорус мгновенного возвращения", "хоме! Хоме! ХОМЕ!", ChatColor.LIGHT_PURPLE, null, 10, 1)
 }
--- src/main/kotlin/me/galaran/nyamine/feature/ReturnChorus.kt
+++ src/main/kotlin/me/galaran/nyamine/feature/ReturnChorus.kt
@@ -3,10 +3,11 @@ package me.galaran.nyamine.feature
 import me.galaran.nyamine.NyaMineFeatures
 import me.galaran.nyamine.ReturnChorusGrade
 import net.ess3.api.IEssentials
-import org.bukkit.Effect
-import org.bukkit.Material
-import org.bukkit.Sound
+import org.bukkit.*
+import org.bukkit.boss.BarFlag
+import org.bukkit.boss.BarStyle
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -21,51 +22,104 @@ class ReturnChorus(
         private val essentials: IEssentials
 ) : Listener {
 
-    private val pendingHomeTeleportPlayers = mutableSetOf<UUID>()
+    data class ScheduledTeleportData(
+        val teleportStartTick: Int,
+        val teleportDelayTicks: Int,
+        val progressBarKey: NamespacedKey?
+    )
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    fun onPlayerItemConsume(event: PlayerItemConsumeEvent) {
-        if (event.player.uniqueId in pendingHomeTeleportPlayers) return
+    private val pendingTeleportPlayers = mutableMapOf<UUID, ScheduledTeleportData>()
 
-        val delayTicks = event.item.getTeleportDelayTicks() ?: return
+    init {
+        plugin.server.scheduler.scheduleSyncRepeatingTask(plugin, {
+            if (pendingTeleportPlayers.isEmpty()) return@scheduleSyncRepeatingTask
 
-        pendingHomeTeleportPlayers += event.player.uniqueId
+            val playersIdsToRemove = mutableListOf<UUID>()
+            val currentServerTick = Bukkit.getCurrentTick()
+            for ((playerId, teleportData) in pendingTeleportPlayers) {
+                if (currentServerTick >= teleportData.teleportStartTick + teleportData.teleportDelayTicks) {
+                    playersIdsToRemove += playerId
 
-        plugin.server.scheduler.scheduleSyncDelayedTask(plugin, {
-            if (pendingHomeTeleportPlayers.remove(event.player.uniqueId)) {
-                if (event.player.isOnline) {
-                    val user = essentials.getUser(event.player)
-                    user.teleport.respawn(null, PlayerTeleportEvent.TeleportCause.COMMAND)
+                    val player: Player? = Bukkit.getPlayer(playerId)
+                    if (player != null && player.isOnline) {
+                        val user = essentials.getUser(player)
+                        user.teleport.respawn(null, PlayerTeleportEvent.TeleportCause.COMMAND)
 
-                    with(event.player.location) {
-                        this.world.playEffect(this, Effect.ENDER_SIGNAL, 0, 128)
-                        this.world.playSound(this, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f)
+                        with(player.location) {
+                            this.world.playEffect(this, Effect.ENDER_SIGNAL, 0, 128)
+                            this.world.playSound(this, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f)
+                        }
+                    }
+                } else if (teleportData.progressBarKey != null) {
+                    val newProgress = (currentServerTick - teleportData.teleportStartTick) / teleportData.teleportDelayTicks.toDouble()
+                    Bukkit.getBossBar(teleportData.progressBarKey)?.apply {
+                        progress = newProgress
                     }
                 }
             }
-        }, delayTicks.toLong())
+
+            playersIdsToRemove.forEach(::removeFromPendingTeleport)
+        }, 0, 1)
     }
 
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    fun onPlayerTeleport(event: PlayerTeleportEvent) {
-        if (event.cause == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT && event.player.uniqueId in pendingHomeTeleportPlayers) {
-            event.isCancelled = true
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onPlayerItemConsume(event: PlayerItemConsumeEvent) {
+        val returnChorusGrade = event.item.getReturnChorusGrade() ?: return
+
+        removeFromPendingTeleport(event.player.uniqueId)
+
+        val progressBarKey: NamespacedKey? = if (returnChorusGrade.progressBarColor != null) {
+            val key = NamespacedKey(plugin, "return-chorus-" + UUID.randomUUID())
+            Bukkit.createBossBar(
+                key,
+                returnChorusGrade.nameColor.toString() + returnChorusGrade.displayName,
+                returnChorusGrade.progressBarColor,
+                BarStyle.SEGMENTED_20,
+                BarFlag.CREATE_FOG
+            ).apply {
+                progress = 0.0
+                addPlayer(event.player)
+            }
+            key
+        } else {
+            null
         }
+        pendingTeleportPlayers[event.player.uniqueId] = ScheduledTeleportData(
+            Bukkit.getCurrentTick(),
+            returnChorusGrade.returnDelayTicks,
+            progressBarKey
+        )
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onPlayerDeath(event: PlayerDeathEvent) {
-        pendingHomeTeleportPlayers -= event.entity.uniqueId
+        removeFromPendingTeleport(event.entity.uniqueId)
     }
 
-    private fun ItemStack.getTeleportDelayTicks(): Int? {
+    private fun removeFromPendingTeleport(playerId: UUID) {
+        val removed: ScheduledTeleportData? = pendingTeleportPlayers.remove(playerId)
+        if (removed?.progressBarKey != null) {
+            Bukkit.getBossBar(removed.progressBarKey)?.removeAll()
+            Bukkit.removeBossBar(removed.progressBarKey)
+        }
+    }
+
+    // Cancel normal chorus fruit mechanic for players, pending teleport
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun onPlayerTeleport(event: PlayerTeleportEvent) {
+        if (event.cause == PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT && event.player.uniqueId in pendingTeleportPlayers) {
+            event.isCancelled = true
+        }
+    }
+
+    private fun ItemStack.getReturnChorusGrade(): ReturnChorusGrade? {
         if (this.type != Material.CHORUS_FRUIT) return null
 
         val effLevel = this.enchantments[Enchantment.DIG_SPEED] ?: return null
         return when {
-            effLevel >= ReturnChorusGrade.INSTANT.enchantLevel -> ReturnChorusGrade.INSTANT.returnDelayTicks
-            effLevel >= ReturnChorusGrade.FAST.enchantLevel -> ReturnChorusGrade.FAST.returnDelayTicks
-            effLevel >= ReturnChorusGrade.COMMON.enchantLevel -> ReturnChorusGrade.COMMON.returnDelayTicks
+            effLevel >= ReturnChorusGrade.INSTANT.enchantLevel -> ReturnChorusGrade.INSTANT
+            effLevel >= ReturnChorusGrade.FAST.enchantLevel -> ReturnChorusGrade.FAST
+            effLevel >= ReturnChorusGrade.COMMON.enchantLevel -> ReturnChorusGrade.COMMON
             else -> null
         }
     }
